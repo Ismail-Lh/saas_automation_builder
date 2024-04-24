4175965982771b7675154fbdb27799ec8c0a8a41
Ismail-Lh <ismailsaad03@gmail.com>
2024-04-24 17:42:31 +0100

    feat: adding @prisma/client and prisma-db.ts configuration files

---
+++ lib/prisma-db.ts
@@ -0,0 +1,15 @@
+import { PrismaClient } from '@prisma/client';
+
+// https://www.prisma.io/docs/orm/more/help-and-troubleshooting/help-articles/nextjs-prisma-client-dev-practices
+const prismaClientSingleton = () => new PrismaClient();
+
+declare global {
+  // eslint-disable-next-line vars-on-top, no-var
+  var prismaGlobal: undefined | ReturnType<typeof prismaClientSingleton>;
+}
+
+const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
+
+export default prisma;
+
+if (process.env.NODE_ENV !== 'production') globalThis.prismaGlobal = prisma;
